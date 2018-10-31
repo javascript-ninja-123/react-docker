dcce2d52a0514264c4a9336273f6cf417901cd73
Sungmin Yi <ssuyi@ucdavis.edu>
2018-10-31 15:12:49 -0700

    s

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            wewe www
+            wewe aa
           </a>
         </header>
       </div>
